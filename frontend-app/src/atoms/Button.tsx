9d48d3f7fd49d505bdf82f704d62e0df2dfaf19f
Jia-A <agrawaljiya2011@gmail.com>
2025-08-18 00:44:19 +0530

    feat: add Button and Input components with type definitions

---
+++ frontend-app/src/atoms/Button.tsx
@@ -0,0 +1,9 @@
+import type { ButtonProps } from "../types"
+
+const Button = ({label, type, ...rest} : ButtonProps) => {
+  return (
+    <button type={type} className='bg-[#02111B] px-4 py-2 rounded-md text-white !mt-6 w-full cursor-pointer' {...rest}>{label}</button>
+  )
+}
+
+export default Button
